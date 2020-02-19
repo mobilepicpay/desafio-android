7961fee827bcbbfcdeafbd27f06b22ef3796f406
Matheus de Mello Queiroz <m.dimello@hotmail.com>
2020-02-19 17:09:26 -0300

    Creates basic tests samples

---
+++ app/src/test/java/com/picpay/desafio/android/ExampleService.kt
@@ -0,0 +1,12 @@
+package com.picpay.desafio.android
+
+class ExampleService(
+    private val service: PicPayService
+) {
+
+    fun example(): List<User> {
+        val users = service.getUsers().execute()
+
+        return users.body() ?: emptyList()
+    }
+}
