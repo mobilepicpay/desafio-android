da3b87d817ffa6fb4ae8b7c39a70387202437e38
Andre Tortolano <andretortolano@gmail.com>
2021-04-14 11:55:36 -0300

    set up CI

--- shared/src/main/java/com/picpay/desafio/android/shared/data/local/PicPayDataBase.kt
+++ shared/src/main/java/com/picpay/desafio/android/shared/data/local/PicPayDataBase.kt
@@ -3,7 +3,7 @@ package com.picpay.desafio.android.shared.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [UserLocal::class], version = 1)
+@Database(entities = [UserLocal::class], version = 1, exportSchema = false)
 abstract class PicPayDataBase : RoomDatabase() {
     abstract fun localDataSource(): PicPayLocalDataSource
 }
