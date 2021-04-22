56d55dfb3744ae3e1b94c0e46c5d93c0b4ac5769
dacruzl2 <Dacruzl4>
2021-04-22 20:58:55 -0300

    refactor module app

--- app/src/main/java/com/picpay/desafio/android/PicPayApplication.kt
+++ app/src/main/java/com/picpay/desafio/android/PicPayApplication.kt
@@ -1,4 +1,28 @@
 package com.picpay.desafio.android
 
-class PicPayApplication {
+import android.app.Application
+import com.picpay.desafio.android.persistance.di.localDataSourceModule
+import com.picpay.desafio.android.rest_picpay.di.restPicpayModule
+import com.picpay.desafio.android.feature_contacts.di.featureContactModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class PicPayApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@PicPayApplication)
+            modules(
+                listOf(
+                    featureContactModule,
+                    localDataSourceModule,
+                    restPicpayModule
+                )
+            )
+        }
+    }
 }
