7dd678aa2aff54dee8a6fd15ee95522fbc4af37f
victor montero <victorhugo@ciandt.com>
2022-03-10 12:14:22 -0300

    retrofit setup ok

---
+++ app/src/main/java/com/picpay/desafio/models/Contact.kt
@@ -0,0 +1,8 @@
+package com.picpay.desafio.models
+
+data class Contact(
+    val id: String,
+    val img: String,
+    val name: String,
+    val username: String
+)
---
+++ app/src/main/java/com/picpay/desafio/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.picpay.desafio.util
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
+    }
+}
