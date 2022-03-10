446104b74e975d7cb41a663dc03c063ed9b972bd
victor montero <victorhugo@ciandt.com>
2022-03-10 11:49:08 -0300

    Navigation between fragments

---
+++ app/src/main/java/com/picpay/desafio/ui/SavedUserFragment.kt
@@ -0,0 +1,7 @@
+package com.picpay.desafio.ui
+
+import androidx.fragment.app.Fragment
+import com.picpay.desafio.android.R
+
+class SavedUserFragment : Fragment(R.layout.fragment_saved_user) {
+}
---
+++ app/src/main/java/com/picpay/desafio/ui/UserListFragment.kt
@@ -0,0 +1,7 @@
+package com.picpay.desafio.ui
+
+import androidx.fragment.app.Fragment
+import com.picpay.desafio.android.R
+
+class UserListFragment : Fragment(R.layout.fragment_user_list) {
+}
