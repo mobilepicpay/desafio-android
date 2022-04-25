88a13d5ca08b0a4e5240a48028bb212d4d2bd22f
Luiz <vegakew675@ovout.com>
2022-04-25 18:42:55 -0400

    Improve dependencies

--- app/src/main/java/com/picpay/desafio/android/App.kt
+++ app/src/main/java/com/picpay/desafio/android/App.kt
@@ -1,19 +1,21 @@
 package com.picpay.desafio.android
 
 import android.app.Application
+import android.os.Debug
 import com.picpay.desafio.android.commons.base.baseModule
 import com.picpay.desafio.android.contacts.di.contactsModule
 import com.picpay.desafio.android.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger()
+            androidLogger(Level.ERROR)
             androidContext(this@App)
             modules(
                 listOf(
