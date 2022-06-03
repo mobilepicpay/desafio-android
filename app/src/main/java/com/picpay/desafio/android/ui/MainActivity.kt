9838f288e79dcb26ca0baeb0d9bc07770a60e7dd
rodolfo <rodolfo.ruyz@gmail.com>
2022-06-03 18:20:08 -0300

    compileSdkVersion para 32, gradle de 5.4.1 para 7.3.3, navigation

---
+++ app/src/main/java/com/picpay/desafio/android/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.picpay.desafio.android.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.picpay.desafio.android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
