308977d1ad2f261e6b2d928577243ff626f70bfb
Guilherme Braga <guilhermebraga.lemobs@gmail.com>
2022-06-14 16:46:32 -0300

    Upgrading dependencies	⬆️ :arrow_up:

--- app/src/main/java/com/picpay/desafio/android/UserListItemViewHolder.kt
+++ app/src/main/java/com/picpay/desafio/android/UserListItemViewHolder.kt
@@ -4,7 +4,6 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.list_item_user.view.*
 
 class UserListItemViewHolder(
     itemView: View
