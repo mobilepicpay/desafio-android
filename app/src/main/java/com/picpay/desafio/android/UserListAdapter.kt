977cf1a29894909afe11b0881d751817d8172125
Guilherme Braga <guilhermebraga.lemobs@gmail.com>
2022-06-14 17:05:35 -0300

    including Jetpack view binding 🐎

--- app/src/main/java/com/picpay/desafio/android/UserListAdapter.kt
+++ app/src/main/java/com/picpay/desafio/android/UserListAdapter.kt
@@ -1,6 +1,5 @@
 package com.picpay.desafio.android
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -20,10 +19,7 @@ class UserListAdapter : RecyclerView.Adapter<UserListItemViewHolder>() {
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListItemViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_item_user, parent, false)
-
-        return UserListItemViewHolder(view)
+        return UserListItemViewHolder.inflate(parent)
     }
 
     override fun onBindViewHolder(holder: UserListItemViewHolder, position: Int) {
--- app/src/main/java/com/picpay/desafio/android/UserListItemViewHolder.kt
+++ app/src/main/java/com/picpay/desafio/android/UserListItemViewHolder.kt
@@ -1,29 +1,40 @@
 package com.picpay.desafio.android
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.picpay.desafio.android.databinding.ListItemUserBinding
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 
-class UserListItemViewHolder(
-    itemView: View
-) : RecyclerView.ViewHolder(itemView) {
+class UserListItemViewHolder(private var binding: ListItemUserBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(user: User) {
-        itemView.name.text = user.name
-        itemView.username.text = user.username
-        itemView.progressBar.visibility = View.VISIBLE
+        binding.name.text = user.name
+        binding.username.text = user.username
+        binding.progressBar.visibility = View.VISIBLE
         Picasso.get()
             .load(user.img)
             .error(R.drawable.ic_round_account_circle)
-            .into(itemView.picture, object : Callback {
+            .into(binding.picture, object : Callback {
                 override fun onSuccess() {
-                    itemView.progressBar.visibility = View.GONE
+                    binding.progressBar.visibility = View.GONE
                 }
 
                 override fun onError(e: Exception?) {
-                    itemView.progressBar.visibility = View.GONE
+                    binding.progressBar.visibility = View.GONE
                 }
             })
+    }
+
+    companion object {
+        fun inflate(parent: ViewGroup) = UserListItemViewHolder(
+            ListItemUserBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 }
