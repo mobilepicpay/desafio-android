8a826e3eed55f26eb6a9b0df8a6f520ade6ae396
Gabriel Lira <liraferreiragabriel@gmail.com>
2022-08-20 16:15:56 -0300

    Removing gson; updating libs

--- app/src/main/java/com/picpay/desafio/android/users/presentation/view/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/users/presentation/view/MainActivity.kt
@@ -7,20 +7,10 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import com.picpay.desafio.android.R
-import com.picpay.desafio.android.users.data.api.UsersApi
-import com.picpay.desafio.android.users.data.api.response.UserResponse
 import com.picpay.desafio.android.users.presentation.view.adapter.UserListAdapter
 import com.picpay.desafio.android.users.presentation.viewmodel.UsersViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import okhttp3.OkHttpClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -29,28 +19,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var progressBar: ProgressBar
     private lateinit var adapter: UserListAdapter
 
-    private val url = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
-
-    private val gson: Gson by lazy { GsonBuilder().create() }
-
-    private val okHttp: OkHttpClient by lazy {
-        OkHttpClient.Builder()
-            .build()
-    }
-
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(url)
-            .client(okHttp)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build()
-    }
-
-    private val service: UsersApi by lazy {
-        retrofit.create(UsersApi::class.java)
-    }
-
-    val usersViewModel: UsersViewModel by viewModels()
+    private val usersViewModel: UsersViewModel by viewModels()
 
     override fun onResume() {
         super.onResume()
@@ -76,24 +45,13 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         }
     }
 
-    private fun fetchUsers() {
-        service.getUsersLegacy()
-            .enqueue(object : Callback<List<UserResponse>> {
-                override fun onFailure(call: Call<List<UserResponse>>, t: Throwable) {
-                    val message = getString(R.string.error)
+    private fun onError() {
+        val message = getString(R.string.error)
 
-                    progressBar.visibility = View.GONE
-                    recyclerView.visibility = View.GONE
+        progressBar.visibility = View.GONE
+        recyclerView.visibility = View.GONE
 
-                    Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT)
-                        .show()
-                }
-
-                override fun onResponse(call: Call<List<UserResponse>>, response: Response<List<UserResponse>>) {
-                    progressBar.visibility = View.GONE
-
-//                    adapter.users = response.body()!!
-                }
-            })
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT)
+            .show()
     }
 }
