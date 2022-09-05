9f0b4ebbe611f59ec8f88192b4671c959d6a2749
valmiraguiar <valmiir.aguiar7@gmail.com>
2022-09-04 23:19:27 -0300

    adding response cache

--- app/src/main/java/com/picpay/desafio/android/di/MainModule.kt
+++ app/src/main/java/com/picpay/desafio/android/di/MainModule.kt
@@ -1,5 +1,6 @@
 package com.picpay.desafio.android.di
 
+import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.picpay.desafio.android.data.repository.ContactRepository
@@ -8,14 +9,22 @@ import com.picpay.desafio.android.data.services.PicPayService
 import com.picpay.desafio.android.domain.useCases.ListContactsUseCase
 import com.picpay.desafio.android.domain.useCases.ListContactsUseCaseImpl
 import kotlinx.coroutines.Dispatchers
-import okhttp3.OkHttpClient
+import okhttp3.*
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object MainModule {
+
+    //CACHE vars
+    private var CACHE_MAX_AGE_VALUE = 5
+    private var CACHE_MAX_STALE_VALUE = 1
+    private var HEADER_PRAGMA = "Pragma"
+    private var HEADER_CACHE_CONTROL = "Cache-Control"
+    private const val CACHE_SIZE = (5 * 1024 * 1024).toLong()
 
     private const val BASE_URL = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
 
@@ -27,14 +36,53 @@ object MainModule {
 
         fun gsonProvider() = GsonBuilder().create()
 
-        fun okHttpProvider() = OkHttpClient.Builder().build()
+        fun cacheProvider(context: Context) = Cache(context.cacheDir, CACHE_SIZE)
+
+        fun cacheInterceptorProvider() = Interceptor { chain ->
+            val request: Request = chain.request()
+            val cacheControl = CacheControl.Builder()
+                .maxAge(CACHE_MAX_AGE_VALUE, TimeUnit.MINUTES)
+                .build()
+            request.newBuilder()
+                .removeHeader(HEADER_PRAGMA)
+                .header(HEADER_CACHE_CONTROL, cacheControl.toString())
+                .build()
+            chain.proceed(request)
+        }
+
+        fun offlineCacheInterceptorProvider() = Interceptor { chain ->
+            try {
+                chain.proceed(chain.request())
+            } catch (e: Exception) {
+                val cacheControl = CacheControl.Builder()
+                    .onlyIfCached()
+                    .maxStale(CACHE_MAX_STALE_VALUE, TimeUnit.DAYS)
+                    .build()
+                val offlineRequest: Request = chain.request().newBuilder()
+                    .cacheControl(cacheControl)
+                    .removeHeader(HEADER_PRAGMA)
+                    .build()
+                chain.proceed(offlineRequest)
+            }
+        }
+
+        fun okHttpProvider(cache: Cache) =
+            OkHttpClient.Builder()
+                .addNetworkInterceptor(cacheInterceptorProvider())
+                .addInterceptor(offlineCacheInterceptorProvider())
+                .cache(cache)
+                .build()
+
 
         single {
             gsonProvider()
         }.bind<Gson>()
         single {
-            okHttpProvider()
-        }.bind<OkHttpClient>()
+            cacheProvider(get())
+        }
+        single {
+            okHttpProvider(get())
+        }.bind()
         single {
             createService<PicPayService>(get(), get())
         }.bind()
