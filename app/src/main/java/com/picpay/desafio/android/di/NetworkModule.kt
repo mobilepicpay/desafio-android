ce792092ef313d99067e13d86efebb0de1cac1ef
u012307 <diego.cotta@xpi.com.br>
2023-01-18 20:28:36 -0300

    Add usecase add viewmodel and viewstate

--- app/src/main/java/com/picpay/desafio/android/di/NetworkModule.kt
+++ app/src/main/java/com/picpay/desafio/android/di/NetworkModule.kt
@@ -1,34 +1,42 @@
-package com.android.post.di.module
+package com.picpay.desafio.android.di
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.picpay.desafio.android.BuildConfig
 import com.picpay.desafio.android.data.source.remote.PicPayService
+import com.picpay.desafio.android.data.source.remote.UserRemoteDataSource
 import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 private const val TIME_OUT = 30L
 
-val NetworkModule = module {
+val networkModule = module {
 
-    single { createService(get()) }
+    single<PicPayService> { createService(get()) }
 
-    single { createRetrofit(get(), get(), BuildConfig.BASE_URL) }
+    single<Retrofit> { createRetrofit(get(), get(), BuildConfig.BASE_URL) }
 
-    single { OkHttpClient.Builder() }
+    single<OkHttpClient> { createOkHttpClient() }
 
     single { GsonBuilder().create() }
+
+    single { UserRemoteDataSource(get()) }
 }
 
-fun createRetrofit(okHttpClient: OkHttpClient, gson: Gson, url: String): Retrofit {
-    return Retrofit.Builder()
+fun createOkHttpClient(): OkHttpClient =
+    OkHttpClient.Builder()
+        .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
+        .readTimeout(TIME_OUT, TimeUnit.SECONDS)
+        .build()
+
+fun createRetrofit(okHttpClient: OkHttpClient, gson: Gson, url: String): Retrofit =
+    Retrofit.Builder()
         .baseUrl(url)
         .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create(gson)).build()
-}
 
-fun createService(retrofit: Retrofit): PicPayService {
-    return retrofit.create(PicPayService::class.java)
-}
+fun createService(retrofit: Retrofit): PicPayService =
+    retrofit.create(PicPayService::class.java)
